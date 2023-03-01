d78bf938c4ba2b48dac53c143bd08ba125bd7ca6
botcr <102783176+botcr@users.noreply.github.com>
2023-03-01 17:19:24 +0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from telebot import types
+import telebot
+ 
+BOT_TOKEN = os.environ['holy_bible_bot']
+ADMIN_ID1 = 
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+old_testament = 'https://i.ibb.co/H7GJrtb/image.png'
+new_testament = 'https://i.ibb.co/wKM8gJy/1.png'
